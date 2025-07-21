fd88c00457b7d1c30175915d2dee22edfd111c42
risalariansyah <risalariansyah0@gmail.com>
2025-07-21 21:12:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const navList = document.getElementById('nav-list');
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('hidden');
 });
+
+// ini untuk pendaftaran Maba
+const form = document.getElementById('formPendaftaran');
+const modal = document.getElementById('modalBerhasil');
+const tutupModal = document.getElementById('tutupModal');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    modal.style.display = 'flex';
+    form.reset(); // Optional: form langsung bersih setelah submit.
+});
+
+tutupModal.addEventListener('click', function () {
+    modal.style.display = 'none';
+});
+
+// end pendaftaran maba
